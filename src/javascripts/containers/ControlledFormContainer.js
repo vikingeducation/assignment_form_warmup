dabaebac1b626acc8bdbe8b0e8cf09d97feb573e
Egle Libby <eglelibby@gmail.com>
2017-05-23 12:34:31 -0400

    working onchange events

--- src/javascripts/containers/ControlledFormContainer.js
+++ src/javascripts/containers/ControlledFormContainer.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import ControlledForm from '../components/ControlledForm';
-import { validateForm } from '../helpers';
+import React, { Component } from "react";
+import ControlledForm from "../components/ControlledForm";
+import { validateForm } from "../helpers";
+import validate from "validate.js";
 
 class ControlledFormContainer extends Component {
   constructor() {
@@ -8,21 +9,57 @@ class ControlledFormContainer extends Component {
     this.state = {
       success: false,
       errors: {},
-      exampleEmail: '',
-      examplePassword: '',
-      exampleURL: ''
+      exampleEmail: "",
+      examplePassword: "",
+      exampleURL: ""
     };
   }
 
+  onChangeEmail = e => {};
+
+  onChangePassword = e => {};
+
+  onChanageURL = e => {};
+
   onChangeInput = e => {
-    let tempForm;
-    // tempForm.exampleEmail = this.state.exampleEmail.length ? this.state.exampleEmail : "email@email.com";
-    // tempForm.examplePassword = this.state.examplePasssword.length ? this.state.examplePassword: "longenoughpasswooord";
-    //
+    let tempConstraints = {};
+    let tempFormData = {};
+    if (e.target.name === "exampleEmail" || this.state.exampleEmail) {
+      tempConstraints.exampleEmail = {
+        presence: true,
+        email: true
+      };
+      tempFormData.exampleEmail = e.target.value;
+    }
+    // tempConstraints.exampleEmail = this.state.exampleEmail.length
+    //   ? this.state.exampleEmail
+    //   : "email@email.com";
+    // tempForm.examplePassword = this.state.examplePasssword.length
+    //   ? this.state.examplePassword
+    //   : "longenoughpasswooord";
+    // tempForm.exampleURL = this.state.exampleURL.length
+    //   ? this.state.exampleURL
+    //   : "http://www.demo.com";
+    const formConstraints = {
+      exampleEmail: {
+        presence: true,
+        email: true
+      },
+      examplePassword: {
+        presence: true,
+        length: { minimum: 12 }
+      },
+      exampleURL: {
+        url: true
+      }
+    };
+    validateForm = formData => {
+      return validate(formData, formConstraints);
+    };
     const errors = validateForm({
-      exampleEmail: this.state.exampleEmail,
-      examplePassword: this.state.examplePassword,
-      exampleURL: this.state.exampleURL
+      exampleEmail: this.state.exampleEmail
+      // examplePassword: this.state.examplePassword,
+      // exampleURL: this.state.exampleURL
     });
 
     this.setState({
@@ -41,10 +78,10 @@ class ControlledFormContainer extends Component {
     });
 
     if (errors) {
-      console.log('Errors', errors);
+      console.log("Errors", errors);
       this.setState({ errors });
     } else {
-      console.log('Success');
+      console.log("Success");
       this.formSuccess();
     }
   };
@@ -54,11 +91,11 @@ class ControlledFormContainer extends Component {
       {
         success: true,
         errors: {},
-        exampleEmail: '',
-        examplePassword: '',
-        exampleURL: ''
+        exampleEmail: "",
+        examplePassword: "",
+        exampleURL: ""
       },
-      () => console.log('Success!')
+      () => console.log("Success!")
     );
   };
 
