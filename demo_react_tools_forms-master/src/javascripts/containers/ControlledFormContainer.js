5ac28fee804f5ad4ecae582d42c44838f6f419f7
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-13 16:01:16 +0000

    form submit validation

---
+++ demo_react_tools_forms-master/src/javascripts/containers/ControlledFormContainer.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import ControlledForm from "../components/ControlledForm";
+import { validateForm } from "../helpers";
+
+class ControlledFormContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      success: false,
+      errors: {},
+      exampleEmail: "",
+      examplePassword: "",
+      exampleURL: ""
+    };
+  }
+
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    if (validateForm(this.state)) {
+      console.log("it works!!!");
+      this.formSuccess();
+    } else {
+      console.log("didnt");
+    }
+    // if (validateForm())
+  };
+
+  formSuccess = () => {
+    this.setState(
+      {
+        success: true,
+        errors: {},
+        exampleEmail: "",
+        examplePassword: "",
+        exampleURL: ""
+      },
+      () => console.log("Success!")
+    );
+  };
+
+  render() {
+    return (
+      <ControlledForm
+        onSubmit={this.onSubmit}
+        onChangeInput={this.onChangeInput}
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default ControlledFormContainer;
