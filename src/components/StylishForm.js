a24be710c2021cb9019b09bfa98b5ee02533ddda
Andrew Senner <me@andrewsenner.com>
2017-09-13 14:01:20 -0400

    finished

--- src/components/StylishForm.js
+++ src/components/StylishForm.js
@@ -1,147 +1,212 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
-} from "material-ui/Card";
-import TextField from "material-ui/TextField";
-import { pink500, green600 } from "material-ui/styles/colors";
-import FontIcon from "material-ui/FontIcon";
-import Badge from "material-ui/Badge";
-import FlatButton from "material-ui/FlatButton";
-import "material-design-icons/iconfont/material-icons.css";
+	Card,
+	CardActions,
+	CardHeader,
+	CardMedia,
+	CardTitle,
+	CardText
+} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import { pink500, green600 } from 'material-ui/styles/colors';
+import FontIcon from 'material-ui/FontIcon';
+import Badge from 'material-ui/Badge';
+import FlatButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import 'material-design-icons/iconfont/material-icons.css';
+
+class SuccessDialog extends Component {
+	render() {
+		const actions = [
+			<FlatButton
+				label="Done"
+				primary={true}
+				keyboardFocused={true}
+				onClick={this.props.handleClose}
+			/>
+		];
+		return (
+			<div>
+				<Dialog
+					title="Form Submitted Successfully!"
+					actions={actions}
+					modal={false}
+					open={this.props.open}
+					onRequestClose={this.props.handleClose}
+				>
+					<h2>
+						Congratulations, you submitted the form and everything was valid.
+						Luckily, everything was valid, because you don't want to know what
+						would've happened if everything wasn't valid.
+					</h2>
+				</Dialog>
+			</div>
+		);
+	}
+}
+
+const Showable = props => {
+	return props.condition ? props.children : null;
+};
 
 const ValidIcon = props => {
-  const color = props.condition ? green600 : pink500;
-  const type = props.condition ? "done" : "error_outline";
-  return (
-    <FontIcon className="material-icons" color={color}>
-      {type}
-    </FontIcon>
-  );
+	const color = props.condition ? green600 : pink500;
+	const type = props.condition ? 'done' : 'error_outline';
+	return (
+		<FontIcon className="material-icons" color={color}>
+			{type}
+		</FontIcon>
+	);
 };
 
 const FormCard = props => {
-  return (
-    <Card>
-      <CardHeader
-        title="Stylish Form"
-        subtitle="An awesome material-ui form!"
-      />
-      <CardTitle
-        title="Complete the form"
-        subtitle="Everything must be valid... OR ELSE!"
-      />
-      <CardText>
-        <form onSubmit={props.submit} style={{ marginLeft: "1em" }}>
-          <div>
-            <TextField
-              name="name"
-              hintText="Your name here..."
-              floatingLabelText="Name"
-              onChange={props.change}
-              value={props.name.text}
-            />
-            <ValidIcon condition={props.name.isValid} />
-          </div>
-          <br />
-          Email:{" "}
-          <input
-            type="text"
-            name="email"
-            onChange={props.change}
-            value={props.email.text}
-          />
-          <span>{props.email.isValid ? "Valid" : "Invalid"}</span>
-          <br />
-          Phone:{" "}
-          <input
-            type="phone"
-            name="phone"
-            onChange={props.change}
-            value={props.phone.text}
-          />
-          <span>{props.phone.isValid ? "Valid" : "Invalid"}</span>
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-      </CardText>
-      <CardActions>
-        <FlatButton label="Action1" />
-        <FlatButton label="Action2" />
-      </CardActions>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardHeader
+				title="Stylish Form"
+				subtitle="An awesome material-ui form!"
+			/>
+			<CardTitle
+				title="Complete the form"
+				subtitle="Everything must be valid... OR ELSE!"
+			/>
+			<CardText>
+				<form onSubmit={props.submit} style={{ marginLeft: '1em' }}>
+					<div>
+						<TextField
+							autoComplete="false"
+							name="name"
+							hintText="Your name here..."
+							floatingLabelText="Name"
+							onChange={props.change}
+							value={props.name.text}
+						/>
+						<Showable condition={props.name.text.length}>
+							<ValidIcon condition={props.name.isValid} />
+						</Showable>
+					</div>
+					<div>
+						<TextField
+							autoComplete="false"
+							name="email"
+							hintText="Your email here..."
+							floatingLabelText="Email"
+							onChange={props.change}
+							value={props.email.text}
+						/>
+						<Showable condition={props.email.text.length}>
+							<ValidIcon condition={props.email.isValid} />
+						</Showable>
+					</div>
+					<div>
+						<TextField
+							autoComplete="false"
+							name="phone"
+							hintText="Your Phone here..."
+							floatingLabelText="Phone"
+							onChange={props.change}
+							value={props.phone.text}
+						/>
+						<Showable condition={props.phone.text.length}>
+							<ValidIcon condition={props.phone.isValid} />
+						</Showable>
+					</div>
+					<br />
+					<CardActions>
+						<RaisedButton
+							onClick={props.successOpen}
+							type="submit"
+							label="Submit"
+							secondary={true}
+						/>
+					</CardActions>
+				</form>
+			</CardText>
+		</Card>
+	);
 };
 
 const validators = {
-  name: /^[a-zA-Z]+$/,
-  email: /^\w+@\w+\.\w+$/,
-  phone: /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/
+	name: /^[a-zA-Z\s]+$/,
+	email: /^\w+@\w+\.\w+$/,
+	phone: /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/
 };
 export default class SerializedForm extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      name: {
-        text: "",
-        isValid: false
-      },
-      email: {
-        text: "",
-        isValid: false
-      },
-      phone: {
-        text: "",
-        isValid: false
-      }
-    };
-  }
+		this.state = {
+			successDialogOpen: false,
+			name: {
+				text: '',
+				isValid: false
+			},
+			email: {
+				text: '',
+				isValid: false
+			},
+			phone: {
+				text: '',
+				isValid: false
+			}
+		};
+	}
 
-  onChange = e => {
-    // Get name.
-    const name = e.target.name;
-    this.setState({
-      [name]: {
-        text: e.target.value,
-        isValid: validators[name].test(e.target.value)
-      }
-    });
-  };
+	successDialogHandleClose = () => {
+		this.setState({ successDialogOpen: false });
+	};
 
-  onSubmit = e => {
-    e.preventDefault();
-    const state = {};
-    let isValid = true;
-    for (let key in this.state) {
-      const val = this.state[key].text;
-      const valid = validators[key].test(val);
-      if (isValid) {
-        isValid = valid;
-      }
-      state[key] = {
-        ...this.state[key],
-        isValid: valid
-      };
-    }
-    if (isValid) {
-      //dispatch to redux here
-    }
-    this.setState(state);
-  };
+	successDialogHandleOpen = () => {
+		this.setState({ successDialogOpen: true });
+	};
 
-  render() {
-    return (
-      <FormCard change={this.onChange} submit={this.onSubmit} {...this.state} />
-    );
-  }
+	onChange = e => {
+		// Get name.
+		const name = e.target.name;
+		this.setState({
+			[name]: {
+				text: e.target.value,
+				isValid: validators[name].test(e.target.value)
+			}
+		});
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+		const state = {};
+		let isValid = true;
+		for (let key in validators) {
+			const val = this.state[key].text;
+			const valid = validators[key].test(val);
+			if (isValid) {
+				isValid = valid;
+			}
+			state[key] = {
+				...this.state[key],
+				isValid: valid
+			};
+		}
+		this.setState(state);
+		if (isValid) {
+			this.successDialogHandleOpen(e);
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<FormCard
+					change={this.onChange}
+					submit={this.onSubmit}
+					{...this.state}
+				/>
+				<SuccessDialog
+					handleClose={this.successDialogHandleClose}
+					open={this.state.successDialogOpen}
+				/>
+			</div>
+		);
+	}
 }
-
-/*
-
- */
