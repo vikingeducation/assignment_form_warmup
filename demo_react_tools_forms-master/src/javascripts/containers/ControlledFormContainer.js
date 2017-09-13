1d11baec61edf5a5ff5543e9acd2105987da0d4d
Eric Glover <etgt28@mail.missouri.edu>
2017-09-13 11:50:34 -0500

    live form validations

--- demo_react_tools_forms-master/src/javascripts/containers/ControlledFormContainer.js
+++ demo_react_tools_forms-master/src/javascripts/containers/ControlledFormContainer.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import ControlledForm from "../components/ControlledForm";
-import { validateForm } from "../helpers";
+import {
+  validateForm,
+  validateEmail,
+  validatePassword,
+  validateURL
+} from "../helpers";
 
 class ControlledFormContainer extends Component {
   constructor() {
@@ -13,23 +18,44 @@ class ControlledFormContainer extends Component {
       exampleURL: ""
     };
   }
-
-  onChangeInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value
+  //USER STORIES
+  //I WANT YOUR FORM NOT TO SUCK
+  //I SHOULDNT HAVE TO TYPE
+  //PREDICTIVE ERROR CORRECTING
+  //
+  inputValidationHash = {
+    exampleEmail: validateEmail,
+    examplePassword: validatePassword,
+    exampleURL: validateURL
+  };
+  onChangeInput = async e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      return null;
     });
+
+    // let validateErrors;
+    // console.log("value = ", e.target.value);
+    let validateErrors = this.inputValidationHash[e.target.name](
+      this.state[e.target.name]
+    );
+
+    if (validateErrors) {
+      this.setState({ errors: validateErrors });
+    } else {
+      this.setState({ errors: {} });
+    }
   };
 
   onSubmit = e => {
     e.preventDefault();
     console.log(this.state);
-    if (validateForm(this.state)) {
-      console.log("it works!!!");
+    let validateErrors;
+    if (!(validateErrors = validateForm(this.state))) {
       this.formSuccess();
     } else {
-      console.log("didnt");
+      console.log("errors = ", validateErrors);
+      this.formError(validateErrors);
     }
-    // if (validateForm())
   };
 
   formSuccess = () => {
@@ -42,6 +68,16 @@ class ControlledFormContainer extends Component {
         exampleURL: ""
       },
       () => console.log("Success!")
+    );
+  };
+
+  formError = errors => {
+    this.setState(
+      {
+        success: false,
+        errors: errors
+      },
+      () => console.log("Errors!!")
     );
   };
 
--- demo_react_tools_forms-master/src/javascripts/helpers.js
+++ demo_react_tools_forms-master/src/javascripts/helpers.js
@@ -1,27 +1,41 @@
-import validate from 'validate.js'
+import validate from "validate.js";
 
 export function getColorFromError(error) {
-  return !!error ? 'danger' : 'default'
+  return !!error ? "danger" : "default";
 }
 
 export function isEmpty(object) {
-  return Object.keys(object).length === 0
+  return Object.keys(object).length === 0;
 }
 
 const formConstraints = {
   exampleEmail: {
     presence: true,
-    email: true,
+    email: true
   },
   examplePassword: {
     presence: true,
-    length: {minimum: 12},
+    length: { minimum: 12 }
   },
   exampleURL: {
-    url: true,
-  },
-}
+    url: true
+  }
+};
 
 export function validateForm(formData) {
-  return validate(formData, formConstraints)
+  return validate(formData, formConstraints);
+}
+
+export function validateEmail(formData) {
+  return validate(formData, { exampleEmail: formConstraints["exampleEmail"] });
+}
+
+export function validatePassword(formData) {
+  return validate(formData, {
+    examplePassword: formConstraints["examplePassword"]
+  });
+}
+
+export function validateURL(formData) {
+  return validate(formData, { exampleURL: formConstraints["exampleURL"] });
 }
